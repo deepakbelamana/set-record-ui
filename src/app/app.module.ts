81793b38024b8a3f9901c849d9d9b446bfb93b31
Belamana Deepak <belamana.10683383@ltimindtree.com>
2023-12-11 18:26:23 +0530

    able to add workout to category

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,23 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button'
-
+import { FormsModule } from '@angular/forms';
+import { WorkOutCategory } from './models/work-out-category';
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    FormsModule, 
   ],
-  providers: [],
+  providers: [WorkOutCategory],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { WorkOutCategory } from '../../models/work-out-category';
+import { WorkOut } from '../../models/work-out';
+import {  OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +10,29 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  enableAddWorkOutInput:boolean = false;
+  newWorkOut:string ="";
+  workOutCategoryList: WorkOutCategory[]=[];
+  workOut: WorkOut = new WorkOut;
+  workOutCategory : WorkOutCategory= new WorkOutCategory;
+  
+  constructor() {
+    this.workOutCategoryList=[
+      {categoryId:"1",categoryName:"chest and Triceps",description:"push day",workOutsList:[]}
+    ]
+  }
+  ngOninit():void {
+   
+  }
+  addWorkOut(workOutCategory:WorkOutCategory) {
+    if(this.newWorkOut === "") {
+      this.enableAddWorkOutInput=!this.enableAddWorkOutInput
+    } else {
+      this.workOut.workOutName=this.newWorkOut;
+      workOutCategory.workOutsList.push(this.workOut);
+      alert("work out added to category")
+      console.log(workOutCategory)
+    }
+    
+  }
 }
